0b27ea640e17cc1ea9ae361481ebfd036dfc78ba
mindulle <mindullestudio@gmail.com>
2026-07-02 08:44:17 +0900

    feat(dsa): implement solutions for valid parentheses, lru cache, and number of islands

--- dsa_training/03_lru_cache/lru_cache.py
+++ dsa_training/03_lru_cache/lru_cache.py
@@ -1,12 +1,19 @@
+from collections import OrderedDict
+
 class LRUCache:
     def __init__(self, capacity: int):
-        # TODO: Implement your initialization here
-        pass
+        self.capacity = capacity
+        self.cache = OrderedDict()
 
     def get(self, key: int) -> int:
-        # TODO: Implement get method
-        pass
+        if key not in self.cache:
+            return -1
+        self.cache.move_to_end(key)
+        return self.cache[key]
 
     def put(self, key: int, value: int) -> None:
-        # TODO: Implement put method
-        pass
+        if key in self.cache:
+            self.cache.move_to_end(key)
+        self.cache[key] = value
+        if len(self.cache) > self.capacity:
+            self.cache.popitem(last=False)
--- dsa_training/04_number_of_islands/number_of_islands.py
+++ dsa_training/04_number_of_islands/number_of_islands.py
@@ -6,5 +6,23 @@ class Solution:
         Given an m x n 2D binary grid which represents a map of '1's (land) and '0's (water),
         return the number of islands.
         """
-        # TODO: Implement your solution here
-        pass
+        if not grid:
+            return 0
+            
+        def dfs(i, j):
+            if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] == '0':
+                return
+            grid[i][j] = '0'
+            dfs(i+1, j)
+            dfs(i-1, j)
+            dfs(i, j+1)
+            dfs(i, j-1)
+            
+        count = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == '1':
+                    dfs(i, j)
+                    count += 1
+                    
+        return count
