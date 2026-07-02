b28a4c50ec9db70a2911d07ae587e222aeb1edc8
mindulle <mindullestudio@gmail.com>
2026-07-02 09:28:58 +0900

    refactor(dsa): standardize two_sum to use class Solution convention

--- dsa_training/01_two_sum/test_two_sum.py
+++ dsa_training/01_two_sum/test_two_sum.py
@@ -1,18 +1,13 @@
 import pytest
-from two_sum import two_sum
+from two_sum import Solution
 
-def test_two_sum_basic():
-    assert sorted(two_sum([2, 7, 11, 15], 9)) == [0, 1]
-
-def test_two_sum_different_order():
-    assert sorted(two_sum([3, 2, 4], 6)) == [1, 2]
-
-def test_two_sum_same_elements():
-    assert sorted(two_sum([3, 3], 6)) == [0, 1]
-
-def test_two_sum_no_solution():
-    assert two_sum([1, 2, 3], 7) == []
-
-def test_two_sum_empty_or_none():
-    assert two_sum([], 9) == []
-    assert two_sum(None, 9) == []
+def test_two_sum():
+    sol = Solution()
+    # Standard cases
+    assert sorted(sol.twoSum([2, 7, 11, 15], 9)) == [0, 1]
+    assert sorted(sol.twoSum([3, 2, 4], 6)) == [1, 2]
+    assert sorted(sol.twoSum([3, 3], 6)) == [0, 1]
+    
+    # Edge cases
+    assert sol.twoSum([], 9) == []
+    assert sol.twoSum([1], 1) == []
